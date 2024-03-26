ec6c889522642c5004c6eeed8443fbff3a3c9b8e
kkpagaev <kkpagaev@gmail.com>
2024-03-27 01:08:01 +0200

    createad empty functions for IO

---
+++ app/io/input.py
@@ -0,0 +1,29 @@
+
+
+def read_prompt():
+    """
+    Reads input from user
+
+    Returns:
+        str: user input
+    """
+    pass
+
+def read_file_builtin():
+    """
+    Reads file using built-in function
+
+    Returns:
+        str: file content
+    """
+    pass
+
+def read_file_pandas():
+    pass
+
+
+
+
+
+
+
---
+++ app/io/output.py
@@ -0,0 +1,22 @@
+
+def puts(text : str):
+    """
+    Prints text to console
+
+    Args:
+        text: text to print
+    """
+    pass
+
+
+def write_to_file(filepath : str, text : str):
+    """
+    Writes text to file
+
+    Args:
+        filepath: path to file
+        text: text to write
+    """
+    pass
+
+
