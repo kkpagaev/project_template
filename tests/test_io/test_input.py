0d964d2d6c62f98fd50b3e3b7b990a083d831b5a
kkpagaev <kkpagaev@gmail.com>
2024-03-27 01:57:10 +0200

    added pandas file read tests

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -1,7 +1,7 @@
 import unittest
 import app.io.input as I
 
-class TestInputModule(unittest.TestCase):
+class TestInputFileReadFromStandardModule(unittest.TestCase):
     def test_read_file_builtin(self):
         """
         Test read_file_builtin
@@ -15,5 +15,21 @@ class TestInputModule(unittest.TestCase):
         """
         with self.assertRaises(FileNotFoundError):
            I.read_file_builtin("./tests/test_io/test_data/total.txt")
+
+class TestInputFileReadFromPandasModule(unittest.TestCase):
+    def test_read_file_pandas(self):
+        """
+        Test read_file_pandas
+        """
+        res = I.read_file_pandas("./tests/test_io/test_data/test.txt")
+        self.assertEqual(res, ",foo\n0,bar\n")
+
+
+    def test_read_pandas_unexisting_file(self):
+        """
+        Test read_uneexistent_file
+        """
+        with self.assertRaises(FileNotFoundError):
+           I.read_file_pandas("./tests/test_io/test_data/total.txt")
 
 
