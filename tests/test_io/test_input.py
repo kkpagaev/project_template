d283239e8478f20082cc3d86a0ae0aadd19ecda2
kkpagaev <kkpagaev@gmail.com>
2024-03-27 01:51:32 +0200

    test case if file does not exist

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -9,4 +9,11 @@ class TestInputModule(unittest.TestCase):
         res = I.read_file_builtin("./tests/test_io/test_data/test.txt")
         self.assertEqual(res, "foo\nbar\n")
 
+    def test_read_unexisting_file(self):
+        """
+        Test read_uneexistent_file
+        """
+        with self.assertRaises(FileNotFoundError):
+           I.read_file_builtin("./tests/test_io/test_data/total.txt")
+
 
